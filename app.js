60dd3faab133d0779850894c6ffffdf15361d9da
CrypticCodeDeveloper <crypticcodetechnologies@gmail.com>
2025-05-07 09:11:22 +0100

    Add custom cors middleware

--- app.js
+++ app.js
@@ -20,6 +20,13 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
 }));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 
 
 var indexRouter = require('./routes/index');
